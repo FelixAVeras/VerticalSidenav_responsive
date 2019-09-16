fff1679f9dc5db673a2c09cc13c91c3d9257ca87
Felix Ant. Carvajal <fcarvajal44@gmail.com>
2019-09-16 10:08:44 -0400

    Creating a Vertical sidebar accordeon

---
+++ js/custom.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+	$('.menu li:has(ul)').click(function(e) {
+		e.preventDefault();
+
+		if($(this).hasClass('activado')) {
+			$(this).removeClass('activado');
+			$(this).children('ul').slideUp();
+		} else {
+			$('.menu li ul').slideUp();
+			$('.menu li').removeClass('activado');
+			$(this).addClass('activado');
+			$(this).children('ul').slideDown();
+		}
+	});
+});
